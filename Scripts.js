e438e68d9ecc73f3a4c5ca264d6662f3e49afffd
Jacky Zheng <jz675@cornell.edu>
2018-12-26 18:06:58 -0500

    made bumping simulator

--- Scripts.js
+++ Scripts.js
@@ -3,9 +3,9 @@ d3.select('h1').style('color', 'white');
 d3.select('h1').style('font-size', '24px');
 
 var NUM = 100; //NUMber of dots that we will have
-var WIDTH = 1000; //WIDTH of screen
-var HEIGHT = 1000; //HEIGHT of screen
-var SPEEDLIM = 10; //speed limitation
+var WIDTH = 200; //WIDTH of screen
+var HEIGHT = 200; //HEIGHT of screen
+var SPEEDLIM = 1; //speed limitation
 var VISION = 9; //how far each unit can see
 var INTROVERT = 1; //how introverted the boids are
 var STEERINGFACTOR = 0.4; //how well does the boid take it's own current value.
@@ -26,12 +26,10 @@ var data = d3.range(NUM).map(() => {
   };
 });
 //appending svg
-d3.select("body")
+var svg = d3.select("body")
     .append("svg")
-    .attr("WIDTH", WIDTH)
-    .attr("HEIGHT", HEIGHT);
-//Select SVG element
-var svg = d3.select("svg");
+    .attr("width", WIDTH)
+    .attr("height", HEIGHT);
 
 //creation of the boids, in our case they are circles. could change to triangles
 circles = svg.selectAll("circle")
@@ -46,10 +44,12 @@ circles = svg.selectAll("circle")
 setInterval(() => {
   // Update the data bound to the circles.
   data.forEach(d => { //take in d (as in data) and do something with it
-    //updating the data don't worry about transitions. Circles are data bound.
+    //updating the data *don't worry about transitions. Circles are data bound.
     d.cx += d.Xvec;
     d.cy += d.Yvec;
     //these are edge cases, replace with modular arithmetic later to make faster
+
+    /*
     if (d.cx > WIDTH) {
       d.cx -= WIDTH;
     } else if(d.cx < 0) {
@@ -59,7 +59,12 @@ setInterval(() => {
       d.cy -= HEIGHT;
     } else if(d.cy < 0) {
       d.cy += HEIGHT;
-    }
+    }*/
+
+    //try
+    //note a mod b = ((a % b) + b) % b
+    d.cx = ((d.cx % WIDTH) + WIDTH) % WIDTH;
+    d.cy = ((d.cy % HEIGHT) + HEIGHT) % HEIGHT;
   })
   // Update the circles' positions according to the data.
   circles.attr("cx", function(d) {return d.cx;})
@@ -67,7 +72,7 @@ setInterval(() => {
           .attr("r", function(d) {return d.r;})
           .attr("fill", function(d) {return d.fill;});
 
-  var modified = []; //stores the changed data. Don't want to modify the data using modified data
+  var modified = []; //stores the changed data. Don't want to have redundencies
   data.forEach(d => { //updating values.
     xval = [];
     yval = [];
@@ -76,64 +81,69 @@ setInterval(() => {
     for (var i = 0; i<NUM; i++) {
       var distY = d.cy - data[i].cy;
       var distX = d.cx - data[i].cx;
-      var distance = (distY^2)+(distX^2);
+      var distance = (distY**2)+(distX**2);
       if (distance < VISION) {
-        yval.push(Ydistance: distY, d.Yvec);
-        xval.push(Xdistance: distX, d.Xvec);
+        yval.push({distY: distY, Yvec: d.Yvec, cy: d.cy});
+        xval.push({distX: distX, Xvec: d.Xvec, cx: d.cx});
+        /* //A way to push things to close together away.
         if (distance < INTROVERT) {
 
         }
+        */
       }
     }
 
-    var vec1 = cohesion(xval, yval);
+    var vec1 = cohesion(xval, yval, d);
+    var vec2 = alignment(xval, yval, d);
+    var summedX = vec1[0] + vec2[0];
+    var summedY = vec1[1] + vec2[1];
 
-    d.Xvec -= vec1[0];
-    d.Yvec -= vec1[1];
-    var magSq = ((d.Xvec^2) + (d.Yvec^2))^(1/2);
-
+    var magSq = ((summedX**2) + (summedY**2));
+    console.log(((-1)**2 + (-1)**2));
     //normalize is there better math? #fixthis ?
-    if (magSq >SPEEDLIM) {
-      d.Xvec = (d.Xvec / magSq) * SPEEDLIM;
-      d.Yvec = (d.Yvec / magSq) * SPEEDLIM;
+    if (magSq > SPEEDLIM) {
+      summedX = (summedX / magSq) * SPEEDLIM;
+      summedY = (summedY / magSq) * SPEEDLIM;
     }
-    //movement based on vectors
-
-    d.cx += d.Xvec;
-    d.cy += d.Yvec;
-    if (d.cx > WIDTH) {
-      d.cx -= WIDTH;
-    } else if(d.cx < 0) {
-      d.cx += WIDTH;
-    }
-    if (d.cy > HEIGHT) {
-      d.cy -= HEIGHT;
-    } else if(d.cy < 0) {
-      d.cy += HEIGHT;
-    }
+    modified.push({Xvec: summedX, Yvec: summedY});
   });
-
-
+  for (var i = 0; i < NUM; i++) {
+    data[i].Xvec = modified[i].Xvec;
+    data[i].Yvec = modified[i].Yvec;
+  }
+  console.log(data);
 }, 50);
 
 
-function cohesion(p1,p2) {
+function cohesion(p1, p2, d) {
     var vec = [0,0]; //0 is x, 1 is y
     var length = p1.length;
+    //if no close by points, return original vectors
     if (length == 0) {
+      vec = [d.Xvec, d.Yvec];
       return vec;
     }
     for (var i = 0; i < length; i++) {
-      vec[0] += p1[i];
-      vec[1] += p2[i];
+      vec[0] += p1[i].distX;
+      vec[1] += p2[i].distY;
     }
     vec[0] = vec[0]/length;
     vec[1] = vec[1]/length;
     return vec;
 }
-function alightment(p1, p2){
-
-}
-function seperation(p1, p2){
-
+function alignment(p1, p2, d){
+  var vec = [0,0]; //0 is x, 1 is y
+  var length = p1.length;
+  //if no close by points, return original vectors
+  if (length == 0) {
+    vec = [d.Xvec, d.Yvec];
+    return vec;
+  }
+  for (var i = 0; i < length; i++) {
+    vec[0] += p1[i].Xvec;
+    vec[1] += p2[i].Yvec;
+  }
+  vec[0] = vec[0]/length;
+  vec[1] = vec[1]/length;
+  return vec;
 }
