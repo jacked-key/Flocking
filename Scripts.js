c6bbe581f820ff3d923902c33c1150f2351276e9
Jacky Zheng <jz675@cornell.edu>
2019-01-11 23:35:18 -0500

    Fixed Problem with Steering

--- Scripts.js
+++ Scripts.js
@@ -2,17 +2,18 @@
 d3.select('h1').style('color', 'white');
 d3.select('h1').style('font-size', '24px');
 
-var NUM = 100; //NUMber of dots that we will have
-var WIDTH = 500; //WIDTH of screen
-var HEIGHT = 500; //HEIGHT of screen
-var SPEEDLIM = 3; //speed limitation
-var VISION = 9000; //how far each unit can see
-var INTROVERT = 1; //how introverted the boids are
-var STEERINGFACTOR = 0.4; //how well does the boid take it's own current value.
-var STEERLIM = 1; //steer lim instead of steeringFactor
-var COHESION = 0.5;
-var ALIGNMENT = 0;
-var SEPSTRENGTH = 0;  //value of seperation is only high when the boids are close
+var NUM = 1000; //NUMber of dots that we will have
+var WIDTH = 1000; //WIDTH of screen
+var HEIGHT = 1000; //HEIGHT of screen
+var SPEEDLIM = 4; //speed limitation #fixthis implement a speed minimum
+var VISION = 900; //how far each unit can see (square rooted)
+var INTROVERT = 25; //how introverted the boids are
+var STEERINGFACTOR = 1; //how well does the boid take it's own current value.
+//REMINDER THAT STEERING IS THE DIFFERENCE IN VECTORS *FIX
+var STEERLIM = 1; //steering max turnage instead of steeringFactor
+var COHESION = 1;
+var ALIGNMENT = 1;
+var SEPSTRENGTH = 1;  //value of seperation is only high when the boids are close
 //currently, the new vectors are 0.4 of currnet, 0.3 of cohesion and 0.3 of allignment
 
 //our 100 boids #Fixthis? use a matrix with linked list to speed up calculation.
@@ -22,8 +23,8 @@ var data = d3.range(NUM).map(() => {
     cy: Math.floor(Math.random() * HEIGHT),
     r: 0.5,
     fill: "white",
-    Xvec: (Math.random()*6)-3, //speed goes from -3 to 3 initially
-    Yvec: (Math.random()*6)-3
+    Xvec: (Math.random()*2*SPEEDLIM)-SPEEDLIM, //speed goes from -STEERLIM to STEERLIM
+    Yvec: (Math.random()*2*SPEEDLIM)-SPEEDLIM
   };
 });
 //appending svg
@@ -54,6 +55,7 @@ setInterval(() => {
     d.cy = ((d.cy % HEIGHT) + HEIGHT) % HEIGHT;
   })
   // Update the circles' positions according to the data.
+
   circles.attr("cx", function(d) {return d.cx;})
           .attr("cy", function(d) {return d.cy;})
           .attr("r", function(d) {return d.r;})
@@ -65,23 +67,23 @@ setInterval(() => {
   data.forEach(d => { //using vision to calulate steering
     xval = [];
     yval = [];
-    var separation = {Xvec: 0, Yvec: 0};
+    var separation = {Xvec: 0, Yvec: 0}; //used for calculating separatoin
     var Closest = HEIGHT;
     //current method of VISION #fixthis using matrix / other thing to make faster.
     //This basically goes through everything which is slow as you can see.
     for (var i = 0; i<NUM; i++) {
-      var distY = d.cy - data[i].cy;
-      var distX = d.cx - data[i].cx;
-      var distance = (distY**2)+(distX**2);
-      if (distance < VISION && distance != 0) {
-        yval.push({distY: distY, Yvec: d.Yvec, cy: d.cy});
-        xval.push({distX: distX, Xvec: d.Xvec, cx: d.cx});
+      var distY = data[i].cy - d.cy;
+      var distX = data[i].cx - d.cx;
+      var distance = ((distY**2)+(distX**2));
+      if (distance < VISION && distance != 0) { //distance != 0
+        yval.push({distY: distY, Yvec: data[i].Yvec, cy: d.cy});
+        xval.push({distX: distX, Xvec: data[i].Xvec, cx: d.cx});
         //#fixthis A way to push things to close together this uses the closest boid
         if (distance < INTROVERT && distance < Closest) {
           Closest = distance;
 
-          separation.Xvec = VISION**(1/2)/(distX+INTROVERT)/20;
-          separation.Yvec = VISION**(1/2)/(distY+INTROVERT)/20;
+          separation.Xvec = INTROVERT/distX/10;
+          separation.Yvec = INTROVERT/distY/10;
         }
       }
     }
@@ -92,33 +94,21 @@ setInterval(() => {
     var summedX = vec1[0]*COHESION + vec2[0]*ALIGNMENT;
     var summedY = vec1[1]*COHESION + vec2[1]*ALIGNMENT;
 
-    var magSq = ((summedX**2) + (summedY**2));
-    //normalize -> is there better math?
-    if (magSq > SPEEDLIM) {
-      summedX = (summedX / magSq) * SPEEDLIM;
-      summedY = (summedY / magSq) * SPEEDLIM;
-    }
-    summedX += separation.Xvec*SEPSTRENGTH;
-    summedY += separation.Yvec*SEPSTRENGTH;
+    summedX -= (separation.Xvec - d.Xvec)*SEPSTRENGTH;
+    summedY -= (separation.Yvec - d.Yvec)*SEPSTRENGTH;
 
     modified.push({Xvec: summedX, Yvec: summedY});
   });
   for (var i = 0; i < NUM; i++) {
-    //#fixthis temp is occasionally NaN
-    var temp = modified[i].Xvec**2 + modified[i].Yvec**2;
-    if (temp > STEERLIM) { //temp == temp if not NaN
-      modified[i].Xvec = (modified[i].Xec / (temp**(1/2)))*STEERLIM;
-      modified[i].Yvec = (modified[i].Yec / (temp**(1/2)))*STEERLIM;
-    }
     data[i].Xvec += modified[i].Xvec*STEERINGFACTOR;
     data[i].Yvec += modified[i].Yvec*STEERINGFACTOR;
-    var magSq = ((data[i].Xvec**2) + (data[i].Yvec**2));
+    var magSq = ((data[i].Xvec**2) + (data[i].Yvec**2))**(1/2);
     if (magSq > SPEEDLIM) {
       data[i].Xvec = (data[i].Xvec / magSq) * SPEEDLIM;
       data[i].Yvec = (data[i].Yvec / magSq) * SPEEDLIM;
     }
   }
-}, 25);
+}, 5);
 
 
 function cohesion(p1, p2, d) {
@@ -126,7 +116,6 @@ function cohesion(p1, p2, d) {
     var length = p1.length;
     //if no close by points, return original vectors
     if (length == 0) {
-      vec = [d.Xvec, d.Yvec];
       return vec;
     }
     for (var i = 0; i < length; i++) {
@@ -135,21 +124,41 @@ function cohesion(p1, p2, d) {
     }
     vec[0] = vec[0]/length;
     vec[1] = vec[1]/length;
+    //steering
+    vec[0] = vec[0] - d.Xvec;
+    vec[1] = vec[1] - d.Yvec;
+    //normalize
+    var magSq = ((vec[0]**2) + (vec[1]**2))**(1/2);
+    if (magSq > STEERLIM) {
+      vec[0] = (vec[0] / magSq) * STEERLIM;
+      vec[1] = (vec[1] / magSq) * STEERLIM;
+    }
     return vec;
 }
 function alignment(p1, p2, d){
   var vec = [0,0]; //0 is x, 1 is y
   var length = p1.length;
   //if no close by points, return original vectors
+
   if (length == 0) {
-    vec = [d.Xvec, d.Yvec];
     return vec;
   }
+
   for (var i = 0; i < length; i++) {
     vec[0] += p1[i].Xvec;
     vec[1] += p2[i].Yvec;
   }
   vec[0] = vec[0]/length;
   vec[1] = vec[1]/length;
+  //steering
+  vec[0] = vec[0] - d.Xvec;
+  vec[1] = vec[1] - d.Yvec;
+  //normalize
+  var magSq = ((vec[0]**2) + (vec[1]**2))**(1/2);
+  if (magSq > STEERLIM) {
+    vec[0] = (vec[0] / magSq) * STEERLIM;
+    vec[1] = (vec[1] / magSq) * STEERLIM;
+  }
   return vec;
+
 }
